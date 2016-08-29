1892e8f1965c2584f67cd745b53a6ee402a9cbc4
LucasGCosta <lucasgabriel.777@gmail.com>
2016-08-29 14:55:14 -0300

    Second Commit
    All the src folder files has been commited

---
+++ src/br/com/matrix/matrix/CondicaoFimMatrix.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.matrix.matrix;
+
+import java.util.HashMap;
+
+import br.com.matrix.evo.suporte.CondicaoFimEvo;
+
+public class CondicaoFimMatrix implements CondicaoFimEvo<HashMap<String, Object>> {
+
+    @Override
+    public boolean test(HashMap<String, Object> t) {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+}
